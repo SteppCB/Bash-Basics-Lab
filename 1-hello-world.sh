baab5dc56b3b9fd637104a8ae004ebfd4a58922e
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-09-09 10:32:32 -0400

    git commit -m "finished test hello world"

---
+++ 1-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Prompt the user to enter their name
+read -p "Enter your name: " name
+
+# Print a personalized greeting message
+echo "Hello, $name! Welcome to the world of bash scripting."
